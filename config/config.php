6c6fe7526a8ce87b38b2f6d91b386399ce7f1621
Ermek <72407274+65ermek@users.noreply.github.com>
2023-03-10 12:19:02 +0100

    Initial commit

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$config = [
+    'components' => [
+        'cache' => 'vendor\libs\cache',
+        'sendmail' => 'vendor\libs\sendmail',
+        'geo' => 'vendor\libs\geo',
+    ],
+];
+return $config;
---
+++ config_db.php
@@ -0,0 +1,7 @@
+<?php
+
+return[
+    'dsn' => 'mysql:host=wm107.wedos.net;dbname=d122177_0ea9e50;charset=utf8',
+    'user' => 'a122177_0ea9e50',
+    'password' => 'u72qABLA',
+];
