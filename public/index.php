f3b46b5298a6db52fbf884bfbd21d75b544e8848
Ermek <72407274+65ermek@users.noreply.github.com>
2023-03-10 18:08:55 +0100

    lesson2

--- public/index.php
+++ public/index.php
@@ -3,14 +3,11 @@ $query = rtrim($_SERVER['QUERY_STRING'], '/');
 
 require '../vendor/core/Router.php';
 require '../vendor/libs/functions.php';
+require '../app/controllers/Main.php';
+require '../app/controllers/Parcels.php';
 
 Router::add('^$', ['controller' => 'Main', 'action' => 'index']);
-Router::add('(?P<controller>[a-z-]+)/(?P<action>[a-z-]+)');
+Router::add('^(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?$');
 
 debug(Router::getRoutes());
-
-if (Router::matchRoute($query)) {
-    debug(Router::getRoute());
-} else {
-    echo '404';
-}
+Router::dispatch($query);
